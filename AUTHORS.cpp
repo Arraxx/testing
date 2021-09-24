ef48f6df2e8329e75f2230fcda9f71587e236447
Aman Kumar <54955755+Arraxx@users.noreply.github.com>
2021-09-25 01:32:05 +0530

    Update AUTHORS.cpp

--- AUTHORS.cpp
+++ AUTHORS.cpp
@@ -1,101 +1,94 @@
-=========================================================================
-                 OpenMS -- Open Source Mass Spectrometry
-=========================================================================
+// C++ program for Merge Sort
+#include <iostream>
+using namespace std;
 
-The following authors contributed to OpenMS at some point. For the 
-authors contributing to a specific piece of code, please refer to
-the authors tag in the respective file header.
- - Ahmed Khalil
- - Alexandra Scherbart
- - Axel Walter
- - Alexandra Zerck
- - Andreas Bertsch
- - Andreas Simon
- - Anton Kriese
- - Anton Pervukhin
- - Bastian Blank
- - Chris Bauer
- - Chris Bielow
- - Christian Ehrlich
- - Clemens Groepl
- - Cornelia Friedle
- - Daniel Jameson
- - Darren Kessner
- - David Wojnar
- - David Voigt
- - Dilek Dere
- - Dimitri Schachmann
- - Dominik Schmitz
- - Douglas McCloskey
- - Emil Paulitz
- - Erhan Kenar
- - Eugen Netz
- - Eva Lange
- - Fabian Aicheler
- - Fabian Kriegel
- - Felix Droop
- - Florian Zeller
- - Frederic Lehnert
- - George Rosenberger
- - Guillaume Belz
- - Hannes Roest
- - Hendrik Brauer
- - Hendrik Weisser
- - Holger Franken
- - Holger Plattfaut
- - Immanuel Luhn
- - Jang Jang Jin 
- - Jihyung Kim
- - Johan Teleman
- - Johannes Junker
- - Johannes Veit
- - Julia Thueringer
- - Juliane Schmachtenberg
- - Julianus Pfeuffer
- - Katharina Albers
- - Knut Reinert
- - Lars Nilse
- - Leon Bichmann
- - Lucia Espona
- - Lukas Mueller
- - Lukas Zimmermann
- - Marcel Schilling
- - Marc Sturm
- - Markus Mueller
- - Martin Langwisch
- - Mateusz Łącki
- - Mathias Walzer
- - Matthew The
- - Matthias Bernt
- - Michał Startek
- - Nico Pfeifer
- - Ole Schulz-Trieglaff
- - Oliver Alka
- - Oliver Kohlbacher
- - Pasquale Domenico Colaianni
- - Patricia Scheil
- - Peter Kunszt
- - Petra Gutenbrunner
- - Rene Hussong
- - Ruben Grünberg
- - Samuel Wein
- - Sandro Andreotti
- - Steffen Sass
- - Stephan Aiche
- - Sven Nahnsen
- - Svetlana Kutuzova
- - Swenja Wagner
- - Taraneh Strunk
- - Timo Sachsenberg
- - Tinatin Kasradze
- - Tom Lukas Lankenau
- - Tom Waschischeck
- - Uwe Schmitt
- - Valentin Noske
- - Vincent Musch
- - Vipul Patel
- - Volker Mosthaf
- - Witold Wolski
- - Xiao Liang
- - Justin Sing
- 
+// Merges two subarrays of array[].
+// First subarray is arr[begin..mid]
+// Second subarray is arr[mid+1..end]
+void merge(int array[], int const left, int const mid, int const right)
+{
+	auto const subArrayOne = mid - left + 1;
+	auto const subArrayTwo = right - mid;
+
+	// Create temp arrays
+	auto *leftArray = new int[subArrayOne],
+		*rightArray = new int[subArrayTwo];
+
+	// Copy data to temp arrays leftArray[] and rightArray[]
+	for (auto i = 0; i < subArrayOne; i++)
+		leftArray[i] = array[left + i];
+	for (auto j = 0; j < subArrayTwo; j++)
+		rightArray[j] = array[mid + 1 + j];
+
+	auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+		indexOfSubArrayTwo = 0; // Initial index of second sub-array
+	int indexOfMergedArray = left; // Initial index of merged array
+
+	// Merge the temp arrays back into array[left..right]
+	while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+		if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+			indexOfSubArrayOne++;
+		}
+		else {
+			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+			indexOfSubArrayTwo++;
+		}
+		indexOfMergedArray++;
+	}
+	// Copy the remaining elements of
+	// left[], if there are any
+	while (indexOfSubArrayOne < subArrayOne) {
+		array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+		indexOfSubArrayOne++;
+		indexOfMergedArray++;
+	}
+	// Copy the remaining elements of
+	// right[], if there are any
+	while (indexOfSubArrayTwo < subArrayTwo) {
+		array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+		indexOfSubArrayTwo++;
+		indexOfMergedArray++;
+	}
+}
+
+// begin is for left index and end is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(int array[], int const begin, int const end)
+{
+	if (begin >= end)
+		return; // Returns recursively
+
+	auto mid = begin + (end - begin) / 2;
+	mergeSort(array, begin, mid);
+	mergeSort(array, mid + 1, end);
+	merge(array, begin, mid, end);
+}
+
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+{
+	for (auto i = 0; i < size; i++)
+		cout << A[i] << " ";
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	auto arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Given array is \n";
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	cout << "\nSorted array is \n";
+	printArray(arr, arr_size);
+	return 0;
+}
+
+// This code is contributed by Mayank Tyagi
+// This code was revised by Joshua Estes
